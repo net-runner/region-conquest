d40d174b5f1122a5fae8f33606cf5db7af8f8a08
Tom <przemek-szczepaniak@tlen.pl>
2019-06-05 14:31:29 +0200

    logging screen upgrade, ingame surroundings animation update

--- static/js3D/Addons3D.js
+++ static/js3D/Addons3D.js
@@ -4,46 +4,8 @@ class Addons3D {
         console.log("Addons3D.js loaded")
     }
     loadingScreenSetup() {
-        let loader = new THREE.TextureLoader();
-        loader.load("imgs/smoke.png", function (texture) {
-            let portalGeo = new THREE.PlaneBufferGeometry(350, 350);
-            let portalMaterial = new THREE.MeshStandardMaterial({
-                map: texture,
-                transparent: true
-            });
-            let smokeGeo = new THREE.PlaneBufferGeometry(1000, 1000);
-            let smokeMaterial = new THREE.MeshStandardMaterial({
-                map: texture,
-                transparent: true
-            });
-            let particle = new THREE.Mesh(portalGeo, portalMaterial);
-            for (let p = 880; p > 250; p--) {
-                let particleClone = particle.clone()
-                particleClone.position.set(
-                    0.5 * p * Math.cos((4 * p * Math.PI) / 180),
-                    0.5 * p * Math.sin((4 * p * Math.PI) / 180),
-                    0.1 * p
-                );
-                particleClone.rotation.z = Math.random() * 360;
-                localData.portalParticles.push(particleClone);
-                game.loadingScreenGroup.add(particleClone);
-            }
-            let particle2 = new THREE.Mesh(smokeGeo, smokeMaterial);
-            for (let p = 0; p < 40; p++) {
-                let particleClone2 = particle2.clone()
-                particleClone2.position.set(
-                    Math.random() * 1000 - 500,
-                    Math.random() * 400 - 200,
-                    25
-                );
-                particleClone2.rotation.z = Math.random() * 360;
-                particleClone2.material.opacity = 0.6;
-                localData.portalParticles.push(particleClone2);
-                game.loadingScreenGroup.add(particleClone2);
-            }
-            addons.particlesAnimate()
-
-        });
+        game.scene.add(game.loadingScreenGroup);
+        addons.particlesAnimate()
     }
     boardAddons() {
         var starsMaterial = new THREE.PointsMaterial({ color: 0x888888 });
@@ -67,6 +29,7 @@ class Addons3D {
     }
     particlesAnimate() {
         var delta = game.clock.getDelta();
+        // console.log(delta)
         if (localData.starParticles) {
             localData.starParticles.forEach(p => {
                 p.position.y -= delta * 180;
@@ -89,6 +52,24 @@ class Addons3D {
             });
             if (Math.random() > 0.9) {
                 game.portalLight.power = 350 + Math.random() * 500;
+            }
+        }
+        if (localData.movingBackground) {
+            localData.testAngles[0] += 0.01
+            localData.testAngles[1] += 0.01
+            localData.testAngles[2] += 0.01
+            if (localData.testAngles[0] > Math.PI * 2) localData.testAngles[0] = 0
+            if (localData.testAngles[1] > Math.PI * 2) localData.testAngles[1] = 0
+            if (localData.testAngles[2] > Math.PI * 2) localData.testAngles[2] = 0
+            for (let i = 0; i < localData.movingBackground.length; i++) {
+                // console.log("moving")
+                if (i < localData.movingBackground.length / 3)
+                    localData.movingBackground[i].position.y += 0.8 * Math.sin(localData.testAngles[0]);
+                else if (i > localData.movingBackground.length / 3 && i < localData.movingBackground.length / 3 * 2)
+                    localData.movingBackground[i].position.y += 0.7 * Math.sin(localData.testAngles[1]);
+                else
+                    localData.movingBackground[i].position.y += 0.9 * Math.sin(localData.testAngles[2]);
+
             }
         }
         // game.renderer.render(game.scene, game.camera);
@@ -124,9 +105,30 @@ class Addons3D {
                     let kolor = "0x" + fade + fade + fade
                     square.material.color.setHex(kolor)
                     game.scene.add(square)
+                    localData.movingBackground.push(square)
+                }
+                else {
+                    let square = square1.clone()
+                    let randomY = Math.random() * 220// + 220
+                    square.position.z = -400 + 100 * i
+                    square.position.x = -400 + 100 * j
+                    square.position.y = -250 - (50 - randomY) / 2
+                    // if (i < 0) { square.position.y = -250 - (begin - i) * 50 }
+                    // if (i > 8) { square.position.y = 1150 - (end + i) * 50 }
+                    // if (j < 0 && i < 0) { square.position.y = square.clone().position.y + (-250 - (begin - j) * 50) / 2 }
+                    // if (j > 8 && i < 0) { square.position.y = square.clone().position.y + (1150 - (end + j) * 50) / 2 }
+                    // let floorRandom = Math.floor(randomY / 2) + 40
+                    // let kolor = "0x" + floorRandom + floorRandom + floorRandom
+                    let fade = Math.floor(Math.random() * 38) + 50
+                    let kolor = "0x" + fade + fade + fade
+                    square.material.color.setHex(kolor)
+                    game.scene.add(square)
+                    localData.movingBackground.push(square)
+                    game.loadingScreenGroup.add(square);
                 }
             }
         }
+        localData.movingBackground.sort(() => Math.random() - 0.5);
         let SpotLightRed = new THREE.SpotLight(0xdd4234, 2, 2000, (Math.PI));
         SpotLightRed.position.y = -200
         SpotLightRed.position.z = -600
