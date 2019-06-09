3147dd922d19fab6194d05eebb361c78f23766d3
Tom <przemek-szczepaniak@tlen.pl>
2019-06-09 13:46:11 +0200

    max characters on login

--- static/js/UI.js
+++ static/js/UI.js
@@ -8,10 +8,19 @@ class UI {
         $("#login1").on("click", function () {
             const nickname = $("#nick1").val();
             const password = $("#pass1").val();
-            if (nickname != "" && password == "") {
-                net.login(nickname)
-            } else if (nickname != "" && password == "") {
-                net.performLogin(nickname, password)
+            const alert1 = $("#maxChar")
+            if (nickname.length > 10) {
+                console.log(alert1)
+                alert1.css("visibility", "visible")
+                setTimeout(() => {
+                    alert1.css("visibility", "hidden")
+                }, 2000);
+            } else {
+                if (nickname != "" && password == "") {
+                    net.login(nickname)
+                } else if (nickname != "" && password == "") {
+                    net.performLogin(nickname, password)
+                }
             }
         })
         $("#reset1").on("click", function () {
@@ -38,7 +47,9 @@ class UI {
     }
     loadingOverlayOpen(alercik) {
         let wait = document.getElementById("awaiting")
+        let waitBox = document.getElementsByClassName("smallBox")[0]
         gameData.playerOrder == 0 ? wait.style.backgroundColor = "#0000ff23" : wait.style.backgroundColor = "#ff000023"
+        gameData.playerOrder == 0 ? waitBox.style.backgroundColor = "#0003c062" : waitBox.style.backgroundColor = "#ff000066"
         wait.style.visibility = "visible"
         document.getElementById("awaitingText").innerHTML = alercik
     }
