1ecccaa6dd53411b1937f1ef5cb2892003b9af0a
Tom <przemek-szczepaniak@tlen.pl>
2019-06-10 12:41:25 +0200

    addons code cleanup

--- static/js3D/Addons3D.js
+++ static/js3D/Addons3D.js
@@ -7,193 +7,6 @@ class Addons3D {
         game.scene.add(game.loadingScreenGroup);
         addons.particlesAnimate()
     }
-    // boardAddons() {
-    // var starsMaterial = new THREE.PointsMaterial({ color: 0x888888 });
-    // var starsGeometry = new THREE.Geometry();;
-    // for (let i = 0; i < 12000; i++) {
-    //     if (i % 2000 == 0) {
-    //         starsGeometry = new THREE.Geometry();
-    //         let starField = new THREE.Points(starsGeometry, starsMaterial);
-    //         localData.starParticles.push(starField)
-    //         // console.log(localData.starParticles)
-    //         starField.position.y = -localData.starParticles.length * 300 + 1000
-    //         starField.position.x = 450
-    //         starField.position.z = 450
-    //         starField.name = i.toString()[0]
-    //         game.scene.add(starField);
-    //     }
-    //     let star = new THREE.Vector3();
-    //     star.x = THREE.Math.randFloatSpread(2000);
-    //     star.y = THREE.Math.randFloatSpread(2000);
-    //     star.z = THREE.Math.randFloatSpread(2000);
-    //     starsGeometry.vertices.push(star);
-    // }
-    // }
-    particlesAnimate() {
-        // var delta = game.clock.getDelta();
-        // console.log(delta)
-        // if (localData.starParticles) {
-        //     localData.starParticles.forEach(p => {
-        //         p.position.y -= delta * 180;
-        //         if (p.position.y < -800) {
-        //             p.position.y = 1000
-        //         }
-        //         if (parseInt(p.name) % 4 == 0) {
-        //             p.rotation.y -= delta * 0.05;
-        //             p.rotation.x -= delta * 0.05;
-        //         }
-        //         else {
-        //             p.rotation.y += delta * 0.05;
-        //             p.rotation.z += delta * 0.05;
-        //         }
-        //     });
-        // }
-        if (localData.startIn[0] && localData.readyToStartIn && gameData.loginStatus != "reconnect") {//game starting - board spawn animation
-            for (let i = 0; i < localData.startIn.length; i++) {
-                for (let j = 0; j < localData.startIn[i].length; j++) {
-                    if (localData.startIn[i][j].position.y > localData.startYpos[i][j]) {
-                        localData.startIn[i][j].translateY(-20)
-                    }
-                    else {
-                        localData.startIn[i][j].translateY(localData.startYpos[i][j] - localData.startIn[i][j].position.y)
-                        if (localData.startInDone[i][j] == false) {
-                            localData.startInCount++
-                            if (localData.startInCount >= 81) {
-                                localData.readyToStartIn = false
-                                game.scene.remove(game.loadingScreenGroup)
-                                player.spawnPlayer(true)
-                                player.spawnPlayer(false)
-                            }
-                            localData.startInDone[i][j] = true
-                        }
-                    }
-                }
-            }
-            for (let i = 0; i < localData.startOut.length; i++) {
-                if (i <= localData.startOutCount) {
-                    localData.startOut[i].translateY(-20)
-                    if (localData.startOut[i].position.y < -180) {
-                        if (localData.startOutDone[i] == false) {
-                            localData.startOutCount++
-                        }
-                        localData.startOutDone[i] = true
-                    }
-                }
-            }
-        }
-        if (localData.movingBackground) {//background animation
-            localData.testAngles[0] += 0.01
-            localData.testAngles[1] += 0.01
-            localData.testAngles[2] += 0.01
-            if (localData.testAngles[0] > Math.PI * 2) localData.testAngles[0] = 0
-            if (localData.testAngles[1] > Math.PI * 2) localData.testAngles[1] = 0
-            if (localData.testAngles[2] > Math.PI * 2) localData.testAngles[2] = 0
-            for (let i = 0; i < localData.movingBackground.length; i++) {
-                if (i < localData.movingBackground.length / 3)
-                    localData.movingBackground[i].position.y -= 0.45 * Math.sin(localData.testAngles[0]);
-                else if (i > localData.movingBackground.length / 3 && i < localData.movingBackground.length / 3 * 2)
-                    localData.movingBackground[i].position.y += 0.35 * Math.sin(localData.testAngles[1]);
-                else
-                    localData.movingBackground[i].position.y += 0.4 * Math.sin(localData.testAngles[2]);
-            }
-        }
-        if (gameData.oponent.lastPos.x != undefined || gameData.oponent.lastPos.z != undefined) {//animation of 5 blocks near player
-            let xz = gameData.oponent.lastPos
-
-            let b3D = localData.board3D
-
-            let b3D_center = b3D[xz.z][xz.x]
-            let b3D_right = b3D[xz.z][xz.x + 1]
-            let b3D_left = b3D[xz.z][xz.x - 1]
-            let b3D_top
-            if (b3D[xz.z + 1]) b3D_top = b3D[xz.z + 1][xz.x]
-            let b3D_down
-            if (b3D[xz.z - 1]) b3D_down = b3D[xz.z - 1][xz.x]
-
-            if (b3D_center != undefined)
-                if (b3D_center.position.y < localData.startYpos[xz.z][xz.x] + 40)
-                    b3D_center.position.y += 1
-            if (b3D_right != undefined)
-                if (b3D_right.position.y < localData.startYpos[xz.z][xz.x + 1] + 20)
-                    b3D_right.position.y += 1
-            if (b3D[xz.z + 1] != undefined)
-                if (b3D_top != undefined)
-                    if (b3D_top.position.y < localData.startYpos[xz.z + 1][xz.x] + 20)
-                        b3D_top.position.y += 1
-            if (b3D_left != undefined)
-                if (b3D_left.position.y < localData.startYpos[xz.z][xz.x - 1] + 20)
-                    b3D_left.position.y += 1
-            if (b3D[xz.z - 1] != undefined)
-                if (b3D_down != undefined)
-                    if (b3D_down.position.y < localData.startYpos[xz.z - 1][xz.x] + 20)
-                        b3D_down.position.y += 1
-        }
-        if (gameData.lastPos.x != undefined || gameData.lastPos.z != undefined) {
-            let xz = gameData.lastPos
-
-            let b3D = localData.board3D
-            let b3D_center = b3D[xz.z][xz.x]
-            let b3D_right = b3D[xz.z][xz.x + 1]
-            let b3D_left = b3D[xz.z][xz.x - 1]
-            let b3D_top
-            if (b3D[xz.z + 1]) b3D_top = b3D[xz.z + 1][xz.x]
-            let b3D_down
-            if (b3D[xz.z - 1]) b3D_down = b3D[xz.z - 1][xz.x]
-
-            let xzOp = gameData.oponent.lastPos
-            if (b3D_center != undefined)
-                if (b3D_center.position.y < localData.startYpos[xz.z][xz.x] + 40)
-                    b3D_center.position.y += 1
-            if (b3D_right != undefined)
-                if (b3D_right.position.y < localData.startYpos[xz.z][xz.x + 1] + 20)
-                    b3D_right.position.y += 1
-            if (b3D[xz.z + 1] != undefined)
-                if (b3D_top != undefined)
-                    if (b3D_top.position.y < localData.startYpos[xz.z + 1][xz.x] + 20)
-                        b3D_top.position.y += 1
-            if (b3D_left != undefined)
-                if (b3D_left.position.y < localData.startYpos[xz.z][xz.x - 1] + 20)
-                    b3D_left.position.y += 1
-            if (b3D[xz.z - 1] != undefined)
-                if (b3D_down != undefined)
-                    if (b3D_down.position.y < localData.startYpos[xz.z - 1][xz.x] + 20)
-                        b3D_down.position.y += 1
-
-            for (let i = 0; i < b3D.length; i++) {
-                for (let j = 0; j < b3D[i].length; j++) {
-                    if (i + "" + j != xz.z + "" + xz.x) {
-                        if (i + "" + j != xzOp.z + "" + xzOp.x) {
-                            if (b3D[i][j].position.y > localData.startYpos[i][j]) localData.board3D[i][j].position.y -= 0.5
-                        }
-                    }
-                }
-            }
-        }
-        if (gameData.board) {//board coloring animation
-            for (let i = 0; i < gameData.board.length; i++) {
-                for (let j = 0; j < gameData.board.length; j++) {
-                    let kolorDefault = localData.startColors[i][j].split(",")
-                    if (gameData.board[i][j].redPoints < gameData.board[i][j].bluePoints) { //blue owner
-                        let owner1 = Math.floor(gameData.board[i][j].bluePoints)
-                        let kolor = parseInt(kolorDefault[2].split(")")[0]) + parseInt(owner1)
-                        let typeColor
-                        gameData.board[i][j].type == "conquered" ? typeColor = parseInt(kolorDefault[1]) + 10 : typeColor = kolorDefault[1]
-                        gameData.board[i][j].type == "conqueror" ? typeColor = parseInt(kolorDefault[1]) - 20 : typeColor = kolorDefault[1]
-                        localData.board3D[i][j].material.color.setStyle(kolorDefault[0] + "," + typeColor + "," + kolor + ")")
-                    }
-                    if (gameData.board[i][j].redPoints > gameData.board[i][j].bluePoints) { //red owner
-                        let owner1 = Math.floor(gameData.board[i][j].redPoints)
-                        let kolor = parseInt(kolorDefault[0].split("(")[1]) + parseInt(owner1)
-                        let typeColor
-                        gameData.board[i][j].type == "conquered" ? typeColor = parseInt(kolorDefault[1]) + 10 : typeColor = kolorDefault[1]
-                        gameData.board[i][j].type == "conqueror" ? typeColor = parseInt(kolorDefault[1]) - 20 : typeColor = kolorDefault[1]
-                        localData.board3D[i][j].material.color.setStyle("rgb(" + kolor + "," + typeColor + "," + kolorDefault[2])
-                    }
-                }
-            }
-        }
-        requestAnimationFrame(addons.particlesAnimate);
-    }
     boardSurroundings() {
         let begin = -10
         let end = 19
@@ -383,5 +196,152 @@ class Addons3D {
     refreshText(order, value) {
         game.scene.remove(localData.scores[order]);
         addons.createText(order, "score", value);
+    }
+    particlesAnimate() {
+        if (localData.startIn[0] && localData.readyToStartIn && gameData.loginStatus != "reconnect") {//game starting - board spawn animation
+            for (let i = 0; i < localData.startIn.length; i++) {
+                for (let j = 0; j < localData.startIn[i].length; j++) {
+                    if (localData.startIn[i][j].position.y > localData.startYpos[i][j]) {
+                        localData.startIn[i][j].translateY(-20)
+                    }
+                    else {
+                        localData.startIn[i][j].translateY(localData.startYpos[i][j] - localData.startIn[i][j].position.y)
+                        if (localData.startInDone[i][j] == false) {
+                            localData.startInCount++
+                            if (localData.startInCount >= 81) {
+                                localData.readyToStartIn = false
+                                game.scene.remove(game.loadingScreenGroup)
+                                player.spawnPlayer(true)
+                                player.spawnPlayer(false)
+                            }
+                            localData.startInDone[i][j] = true
+                        }
+                    }
+                }
+            }
+            for (let i = 0; i < localData.startOut.length; i++) {
+                if (i <= localData.startOutCount) {
+                    localData.startOut[i].translateY(-20)
+                    if (localData.startOut[i].position.y < -180) {
+                        if (localData.startOutDone[i] == false) {
+                            localData.startOutCount++
+                        }
+                        localData.startOutDone[i] = true
+                    }
+                }
+            }
+        }
+        if (localData.movingBackground) {//background animation
+            localData.testAngles[0] += 0.01
+            localData.testAngles[1] += 0.01
+            localData.testAngles[2] += 0.01
+            if (localData.testAngles[0] > Math.PI * 2) localData.testAngles[0] = 0
+            if (localData.testAngles[1] > Math.PI * 2) localData.testAngles[1] = 0
+            if (localData.testAngles[2] > Math.PI * 2) localData.testAngles[2] = 0
+            for (let i = 0; i < localData.movingBackground.length; i++) {
+                if (i < localData.movingBackground.length / 3)
+                    localData.movingBackground[i].position.y -= 0.45 * Math.sin(localData.testAngles[0]);
+                else if (i > localData.movingBackground.length / 3 && i < localData.movingBackground.length / 3 * 2)
+                    localData.movingBackground[i].position.y += 0.35 * Math.sin(localData.testAngles[1]);
+                else
+                    localData.movingBackground[i].position.y += 0.4 * Math.sin(localData.testAngles[2]);
+            }
+        }
+        if (gameData.oponent.lastPos.x != undefined || gameData.oponent.lastPos.z != undefined) {//animation of 5 blocks near player
+            let xz = gameData.oponent.lastPos
+
+            let b3D = localData.board3D
+
+            let b3D_center = b3D[xz.z][xz.x]
+            let b3D_right = b3D[xz.z][xz.x + 1]
+            let b3D_left = b3D[xz.z][xz.x - 1]
+            let b3D_top
+            if (b3D[xz.z + 1]) b3D_top = b3D[xz.z + 1][xz.x]
+            let b3D_down
+            if (b3D[xz.z - 1]) b3D_down = b3D[xz.z - 1][xz.x]
+
+            if (b3D_center != undefined)
+                if (b3D_center.position.y < localData.startYpos[xz.z][xz.x] + 40)
+                    b3D_center.position.y += 1
+            if (b3D_right != undefined)
+                if (b3D_right.position.y < localData.startYpos[xz.z][xz.x + 1] + 20)
+                    b3D_right.position.y += 1
+            if (b3D[xz.z + 1] != undefined)
+                if (b3D_top != undefined)
+                    if (b3D_top.position.y < localData.startYpos[xz.z + 1][xz.x] + 20)
+                        b3D_top.position.y += 1
+            if (b3D_left != undefined)
+                if (b3D_left.position.y < localData.startYpos[xz.z][xz.x - 1] + 20)
+                    b3D_left.position.y += 1
+            if (b3D[xz.z - 1] != undefined)
+                if (b3D_down != undefined)
+                    if (b3D_down.position.y < localData.startYpos[xz.z - 1][xz.x] + 20)
+                        b3D_down.position.y += 1
+        }
+        if (gameData.lastPos.x != undefined || gameData.lastPos.z != undefined) {
+            let xz = gameData.lastPos
+
+            let b3D = localData.board3D
+            let b3D_center = b3D[xz.z][xz.x]
+            let b3D_right = b3D[xz.z][xz.x + 1]
+            let b3D_left = b3D[xz.z][xz.x - 1]
+            let b3D_top
+            if (b3D[xz.z + 1]) b3D_top = b3D[xz.z + 1][xz.x]
+            let b3D_down
+            if (b3D[xz.z - 1]) b3D_down = b3D[xz.z - 1][xz.x]
+
+            let xzOp = gameData.oponent.lastPos
+            if (b3D_center != undefined)
+                if (b3D_center.position.y < localData.startYpos[xz.z][xz.x] + 40)
+                    b3D_center.position.y += 1
+            if (b3D_right != undefined)
+                if (b3D_right.position.y < localData.startYpos[xz.z][xz.x + 1] + 20)
+                    b3D_right.position.y += 1
+            if (b3D[xz.z + 1] != undefined)
+                if (b3D_top != undefined)
+                    if (b3D_top.position.y < localData.startYpos[xz.z + 1][xz.x] + 20)
+                        b3D_top.position.y += 1
+            if (b3D_left != undefined)
+                if (b3D_left.position.y < localData.startYpos[xz.z][xz.x - 1] + 20)
+                    b3D_left.position.y += 1
+            if (b3D[xz.z - 1] != undefined)
+                if (b3D_down != undefined)
+                    if (b3D_down.position.y < localData.startYpos[xz.z - 1][xz.x] + 20)
+                        b3D_down.position.y += 1
+
+            for (let i = 0; i < b3D.length; i++) {
+                for (let j = 0; j < b3D[i].length; j++) {
+                    if (i + "" + j != xz.z + "" + xz.x) {
+                        if (i + "" + j != xzOp.z + "" + xzOp.x) {
+                            if (b3D[i][j].position.y > localData.startYpos[i][j]) localData.board3D[i][j].position.y -= 0.5
+                        }
+                    }
+                }
+            }
+        }
+        if (gameData.board) {//board coloring animation
+            for (let i = 0; i < gameData.board.length; i++) {
+                for (let j = 0; j < gameData.board.length; j++) {
+                    let kolorDefault = localData.startColors[i][j].split(",")
+                    if (gameData.board[i][j].redPoints < gameData.board[i][j].bluePoints) { //blue owner
+                        let owner1 = Math.floor(gameData.board[i][j].bluePoints)
+                        let kolor = parseInt(kolorDefault[2].split(")")[0]) + parseInt(owner1)
+                        let typeColor
+                        gameData.board[i][j].type == "conquered" ? typeColor = parseInt(kolorDefault[1]) + 10 : typeColor = kolorDefault[1]
+                        gameData.board[i][j].type == "conqueror" ? typeColor = parseInt(kolorDefault[1]) - 20 : typeColor = kolorDefault[1]
+                        localData.board3D[i][j].material.color.setStyle(kolorDefault[0] + "," + typeColor + "," + kolor + ")")
+                    }
+                    if (gameData.board[i][j].redPoints > gameData.board[i][j].bluePoints) { //red owner
+                        let owner1 = Math.floor(gameData.board[i][j].redPoints)
+                        let kolor = parseInt(kolorDefault[0].split("(")[1]) + parseInt(owner1)
+                        let typeColor
+                        gameData.board[i][j].type == "conquered" ? typeColor = parseInt(kolorDefault[1]) + 10 : typeColor = kolorDefault[1]
+                        gameData.board[i][j].type == "conqueror" ? typeColor = parseInt(kolorDefault[1]) - 20 : typeColor = kolorDefault[1]
+                        localData.board3D[i][j].material.color.setStyle("rgb(" + kolor + "," + typeColor + "," + kolorDefault[2])
+                    }
+                }
+            }
+        }
+        requestAnimationFrame(addons.particlesAnimate);
     }
 }
